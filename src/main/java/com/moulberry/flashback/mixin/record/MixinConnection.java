9d59df07e267c097b94d7363af36953cacc67434
DoritosSpR <122171434+DoritosSpR@users.noreply.github.com>
2026-03-01 15:43:22 +0000

    Update MixinConnection.java

--- src/main/java/com/moulberry/flashback/mixin/record/MixinConnection.java
+++ src/main/java/com/moulberry/flashback/mixin/record/MixinConnection.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Connection.class)
+@Mixin(value = Connection.class, priority = 2000)
 public class MixinConnection {
 
     @Inject(method = "genericsFtw", at = @At("HEAD"))
